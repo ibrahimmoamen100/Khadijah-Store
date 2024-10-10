c9e254fcb1ed2ab6236c04a4b94a6610e9e125ed
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2024-10-10 16:15:00 +0300

    new update

--- components/accordion-qustuin.tsx
+++ components/accordion-qustuin.tsx
@@ -9,22 +9,57 @@ export function AccordionQustion() {
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>
+          ممكن اخذ السلعه بعد ما ادفع المقدم و اول قسط
+        </AccordionTrigger>
         <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
+          لا بتقدر تاخد السلعه بعد ما تخلص كل الاقساط , والحاجه بتكون في المخزن
+          عندنا في الحفظ , ممكن تيجي تعاينها في اي وقت وتطمن عليها
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionTrigger>ما فائده نظام القسط ؟</AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+          اكنك بتعمل جمعيه علشان تشتري مثلا تلاجه , لو مثلا مش بتقدر تحوش ,احنا
+          بنسعدك في ده , وطبعا بدل ما تشتري
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionTrigger>
+          ليه لازم اشتري بالتقسيط ومحولش احوش الفلوس مع نفسي{" "}
+        </AccordionTrigger>
         <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
+          {" "}
+          لان ببساطه علشان تحوش الفلوس وتروح تشتري المنتج هتلاقي السلعه بقي
+          ثمنها الضعف في ظل موجه الاسعار الي مش مظبوطه وتحرير العمله{" "}
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>التقسيط عندكم بيبدأ من كام</AccordionTrigger>
+        <AccordionContent>
+          {" "}
+          بيبدا من اول 2000 جنيه ولكن التفسيط بيكون علي شهرين فقط
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>
+          {" "}
+          هل ممكن نظام التفسيط يساعدني في تجهييز بناتي{" "}
+        </AccordionTrigger>
+        <AccordionContent>
+          {" "}
+          بالفعل ديه افضل طريقه تجهز بناتك و خاصتا مع نظام التفسيط الي عملينو
+          اصلا علشان يساعد العائلات
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>
+          {" "}
+          عندي اسأله تانيه و مش مطروحها عندكم{" "}
+        </AccordionTrigger>
+        <AccordionContent>
+          {" "}
+          تقدر تتواصل معانا علي الواتص واحنا نجوبك
         </AccordionContent>
       </AccordionItem>
     </Accordion>
