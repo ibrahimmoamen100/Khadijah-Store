216cd904d49424df3c461c15ba2dfd6bfaa05a58
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2024-12-01 15:47:32 +0200

    new update

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -38,7 +38,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({ params }) => {
             </div> */}
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-2 gap-y-8 gap-x-4 sm:grid-cols-3 md:grid-cols-4 gap-4">
                 {filteredProducts.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
